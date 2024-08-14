e8f34015aae1d771f38303f4d64f1d28075dec5f
CezarDev <bacanieskidev@gmail.com>
2024-08-14 08:48:44 -0300

    feat: component titulo criado

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,12 +1,16 @@
 import Banner from 'components/Banner';
 import Cabecalho from 'components/Cabecalho';
 import Footer from 'components/Footer';
+import Titulo from 'components/Titulo';
 
 function Inicio() {
   return (
     <>
       <Cabecalho />
       <Banner imagem='home' />
+      <Titulo>
+        <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+      </Titulo>
       <Footer />
     </>
   );
