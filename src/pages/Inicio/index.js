1318d7699814e2066533eff3c492ad9250ae9db8
CezarDev <bacanieskidev@gmail.com>
2024-08-14 08:40:12 -0300

    feat: component banner criado

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,3 +1,4 @@
+import Banner from 'components/Banner';
 import Cabecalho from 'components/Cabecalho';
 import Footer from 'components/Footer';
 
@@ -5,6 +6,7 @@ function Inicio() {
   return (
     <>
       <Cabecalho />
+      <Banner imagem='home' />
       <Footer />
     </>
   );
